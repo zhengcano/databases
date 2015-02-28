9e858d7454a5091eaccc61ccdf1a67b9db0e1b18
zhengcano <zhengcano@gmail.com>
2015-02-27 16:21:07 -0800

    Add: check for username and insert new username

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,18 +1,67 @@
 var models = require('../models');
 var bluebird = require('bluebird');
-
-
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  user: "root",
+  password: "",
+  database: "chat"
+});
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      var message = '';
+      req.on('data', function(data){
+        message += data;
+      });
+      req.on('end', function(){
+        connection.connect();
+        var user, room;
+        connection.query('SELECT username FROM users WHERE username =' + message.username, function(err, result){
+          if (err){
+            connection.query('INSERT INTO users SET ?', {username: message.username}, function(err, result){
+              user = result.insertId;
+            });
+            //add user
+            //get userid
+          } else {
+            connection.query()
+            // dont add and get existing id
+          }
+        });
+        connection.query('', {}, function(){});
+        connection.query('INSERT INTO messages SET ?', {message: message.text}, function(){});
+
+
+      });
+      //Take data from the request
+      //  With the data:
+      //    Add the message to the message table
+      //
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+
+    },
+    post: function (req, res) {
+      connection.query('SELECT username FROM users WHERE ?', {username: req.body.username}, function(err, result){
+        if (err){
+          connection.query('INSERT INTO users SET ?', {username: req.body.username}, function(err, result){
+          });
+          //add user
+          //get userid
+        } else {
+          // dont add and get existing id
+          console.log('name exists');
+        }
+      });
+    }
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -4,15 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
-  username varchar(20),
   message varchar(140),
-  roomname varchar(20)
+  messageid INT AUTO_INCREMENT PRIMARY KEY,
+  userid INT(3),
+  roomid INT(3)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  username varchar(20),
+  userid INT AUTO_INCREMENT PRIMARY KEY
+);
 
-
-
+CREATE TABLE rooms (
+  roomname varchar(20),
+  userid INT AUTO_INCREMENT PRIMARY KEY
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
